cbec90ab66f6047ecc17d60d6c9c3fa55c8a42a8
JamilW <jamilweeks@yahoo.com>
2019-01-28 19:46:04 -0500

    may need to tweak time and styling but looking complete

--- assets/javascript/Moment.js
+++ assets/javascript/Moment.js
@@ -17,8 +17,12 @@ var trainName;
 var destination;
 var firstTrain;
 var frequency;
-date = new moment("date", "MM/DD/YYYY");
-//var currentTime = moment().format("MMMM Do YYYY, hh:mm:ss a");
+
+// Empties values in tables to take in user input
+$("#exampleInputTrain1").val("");
+$("#exampleInputCity1").val("");
+$("#exampleInputTime1").val("");
+$("#exampleInputFrequency1").val("");
 
 // Event handler for retaining train info on screen upon click of submit button
 $(".submit-form").on("click", function(event)    {
@@ -27,26 +31,44 @@ $(".submit-form").on("click", function(event)    {
 // Values to be stored based on user input    
     trainName = $("#exampleInputTrain1").val().trim();
     destination = $("#exampleInputCity1").val().trim();
-    firstTrain = $("#exampleInputTime1").val().trim();
+    firstTrain = moment($("#exampleInputTime1").val().trim(), "HH:mm a").format("X");
     frequency = $("#exampleInputFrequency1").val().trim();
-        console.log(trainName);
+    console.log(trainName);
+
 // Updates in Firebase
     var trainInfo = ({
         trainName: trainName,
         destination: destination,
         firstTrain: firstTrain,
         frequency: frequency,
-//    time = firebase.database().ServerValue.TIMESTAMP
-//  minutesAway: nextArrival - currentTime
     });
 
     database.ref().push(trainInfo);
 });
         database.ref().on("child_added", function(snapshot)   {
-            console.log(snapshot.val());
-    
+        console.log(snapshot.val());
+
+// Values to be stored in database        
         trainName = snapshot.val().trainName;
         destination = snapshot.val().destination;
         firstTrain = snapshot.val().firstTrain;
         frequency = snapshot.val().frequency;
+
+// Variables to calculate next arrival in military time and how many minutes away
+        var nextArrivalPretty = moment.unix(firstTrain).format("HH:mm a");
+        var minutesAway = moment().diff(moment(firstTrain, "X"), "minutes");
+        var nextArrival = minutesAway - frequency;
+        nextArrival = moment().format("HH:mm a");
+
+// Row to be added with stored user input
+    var rowAdd = $("<tr>").append(
+        $("<td id='train-name'>").text(trainName),
+        $("<td id='train-city'>").text(destination),
+        $("<td id='frequency'>").text(frequency),
+        $("<td id='next-arrival'>").text(nextArrival),
+        $("<td id='minutes-away'>").text(minutesAway)
+    );
+
+// Appending user input to row
+        $("#train-table > tbody").append(rowAdd);
     });
